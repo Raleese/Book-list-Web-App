75323a761c423c3421f52057042a70f7837fa4bc
Titas <titaslr38@gmail.com>
2025-08-10 16:40:50 +0300

    Added better styling to the web page

--- BooksWebApp/components/SearchBar.jsx
+++ BooksWebApp/components/SearchBar.jsx
@@ -1,8 +1,14 @@
+import '../styles/SearchBar.css';
+import { useState } from 'react';
+
 function SearchBar(){
+    const [message, setMessage] = useState('');
+
     return(
         <form className='search-form'>
             <label className='search-label'>Search for books</label>
-            <input className='search-input' type='text' placeholder='Harry Potter and the Half-Blooded Prince'></input>
+            <input className='search-input' type='text' placeholder='Harry Potter and the Half-Blooded Prince' onChange={(t) => setMessage(t.target.value)}></input>
+            <p>{message}</p>
         </form>
     )
 }
